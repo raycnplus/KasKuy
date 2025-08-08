33358a412068d706c9a6467af777efcb034c7889
JcodeTeam <pujamasjoo@gmail.com>
2025-08-08 11:09:36 +0700

    flutter: test

--- mobile/lib/main.dart
+++ mobile/lib/main.dart
@@ -13,6 +13,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
 
       debugShowCheckedModeBanner: false,
+      
       // home: LoginPage(),
       // routes: {
       //   '/login': (context) => LoginPage(),
